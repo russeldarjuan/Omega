c8c947f784adcae420a3192af318b5dfb99bbea9
russeldarjuan <69448059+russeldarjuan@users.noreply.github.com>
2020-08-10 13:43:47 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyBTZCEWqpM2anca6_khvbfxUsZYWfYoXP8",
+  authDomain: "bitcoin-miner-v1.firebaseapp.com",
+  databaseURL: "https://bitcoin-miner-v1.firebaseio.com",
+  projectId: "bitcoin-miner-v1",
+  storageBucket: "bitcoin-miner-v1.appspot.com",
+  messagingSenderId: "197118260196",
+  appId: "1:197118260196:web:a51de9e348e3812c19cdf1",
+  measurementId: "G-CETJHWPJ9D"
 };
 
      
